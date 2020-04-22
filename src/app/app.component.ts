f25f9bde455fb6789031786e0db7819cc4a70a04
Felix Kleine Bösing <felix.boesing@t-online.de>
2020-04-22 23:16:46 +0200

    ok, i see. width of sidenav need to be set inside component and then via ngStyle set inside html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,31 +16,23 @@ Quill.register(block /* or NewBlock */, true);
 })
 export class AppComponent {
   title = 'misc-app';
-  opened = true;
-  // TODO Sidenav should collaps if no one hovers over and only shows icons otherwise
+  sideNavWidth = 250;
+  
   @ViewChild('sidenav') sidenav: MatSidenav;
 
+  constructor() {
+
+  }
+
   ngOnInit() {
-    this.opened = window.innerWidth < 768 ? false : true;
-    this.resize(window);
   }
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.resize(event.target);
+  expand() {
+    this.sideNavWidth = 205;
   }
 
-  resize(window_) {
-    this.opened = window_.innerWidth < 768 ? false : true;
-  }
-
-  isBiggerScreen() {
-    const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (width < 768) {
-      return true;
-    } else {
-      return false;
-    }
+  shrink() {
+    this.sideNavWidth = 250;
   }
 
 }
